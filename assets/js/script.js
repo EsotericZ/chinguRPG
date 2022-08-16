06f82eb0030030b3dd62480bc56580492d212364
EsotericZ <cjsand03@gmail.com>
2022-08-16 15:23:54 -0600

    created mage element and dynamically add to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// CHARACTER SELECTION
 let characterPosition = 0;
 characterCarousel(characterPosition);
 
@@ -33,3 +34,23 @@ function characterCarousel(n) {
         dots[2].classList.add('active');
     }
 }
+
+// CHARACTER PROPERTIES - MAGE
+let mage = {
+    health: 80,
+    attack: [15,16,17,18,19,20],
+    attackMod: 1.75,
+    defense: [6,7,8],
+    defenseMod: 1.5,
+}
+
+let mageHealth = document.getElementById("mageHealth");
+let mageAttack = document.getElementById("mageAttack");
+let mageAttackMod = document.getElementById("mageAttackMod");
+let mageDefense = document.getElementById("mageDefense");
+let mageDefenseMod = document.getElementById("mageDefenseMod");
+mageHealth.textContent = `Health: ${mage.health}`;
+mageAttack.textContent = `Attack: ${mage.attack[0]} - ${mage.attack[mage.attack.length - 1]}`;
+mageAttackMod.textContent = `Attack Mod: ${mage.attackMod}`;
+mageDefense.textContent = `Defense: ${mage.defense[0]} - ${mage.defense[mage.defense.length - 1]}`;
+mageDefenseMod.textContent = `Defense Mod: ${mage.defenseMod}`;
