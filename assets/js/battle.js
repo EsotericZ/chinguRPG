78238fb5486c06a685c4cf646bb498bff0bb3598
EsotericZ <cjsand03@gmail.com>
2022-09-01 20:55:28 -0600

    hide button after click

--- assets/js/battle.js
+++ assets/js/battle.js
@@ -40,7 +40,7 @@ attack.addEventListener('click', function() {
 
 mega.addEventListener('click', function() {
     alert('Mega Attack!')
-    console.log('Yea!')
+    mega.classList.add('hidden');
 })
 
 defense.addEventListener('click', function() {
